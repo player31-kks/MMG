b842ec542341628468520ced251202c39665db57
교석 금 <keumks789@naver.com>
2025-09-25 08:04:45 +0900

    fix : Save Request Dialog ui 수정

--- Views/SaveRequestDialog.cs
+++ Views/SaveRequestDialog.cs
@@ -1,83 +1,28 @@
 using System.Windows;
 
-namespace MMG.ViewModels
+namespace MMG.Views
 {
-    public class SaveRequestDialog : Window
+    public partial class SaveRequestDialog : Window
     {
         public string RequestName { get; private set; } = "";
 
         public SaveRequestDialog()
         {
-            Title = "요청 저장";
-            Width = 400;
-            Height = 150;
-            WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            ResizeMode = ResizeMode.NoResize;
+            InitializeComponent();
+            Loaded += (s, e) => RequestNameTextBox.Focus();
+        }
 
-            var grid = new System.Windows.Controls.Grid();
-            grid.RowDefinitions.Add(new System.Windows.Controls.RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
-            grid.RowDefinitions.Add(new System.Windows.Controls.RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
-            grid.RowDefinitions.Add(new System.Windows.Controls.RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            RequestName = RequestNameTextBox.Text;
+            DialogResult = true;
+            Close();
+        }
 
-            var label = new System.Windows.Controls.Label
-            {
-                Content = "요청 이름:",
-                Margin = new Thickness(10)
-            };
-            System.Windows.Controls.Grid.SetRow(label, 0);
-            grid.Children.Add(label);
-
-            var textBox = new System.Windows.Controls.TextBox
-            {
-                Margin = new Thickness(10, 0, 10, 10),
-                Height = 25
-            };
-            System.Windows.Controls.Grid.SetRow(textBox, 1);
-            grid.Children.Add(textBox);
-
-            var buttonPanel = new System.Windows.Controls.StackPanel
-            {
-                Orientation = System.Windows.Controls.Orientation.Horizontal,
-                HorizontalAlignment = HorizontalAlignment.Right,
-                Margin = new Thickness(10)
-            };
-
-            var okButton = new System.Windows.Controls.Button
-            {
-                Content = "저장",
-                Width = 75,
-                Height = 25,
-                Margin = new Thickness(0, 0, 10, 0),
-                IsDefault = true
-            };
-            okButton.Click += (s, e) =>
-            {
-                RequestName = textBox.Text;
-                DialogResult = true;
-                Close();
-            };
-
-            var cancelButton = new System.Windows.Controls.Button
-            {
-                Content = "취소",
-                Width = 75,
-                Height = 25,
-                IsCancel = true
-            };
-            cancelButton.Click += (s, e) =>
-            {
-                DialogResult = false;
-                Close();
-            };
-
-            buttonPanel.Children.Add(okButton);
-            buttonPanel.Children.Add(cancelButton);
-            System.Windows.Controls.Grid.SetRow(buttonPanel, 2);
-            grid.Children.Add(buttonPanel);
-
-            Content = grid;
-
-            Loaded += (s, e) => textBox.Focus();
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
         }
     }
 }
